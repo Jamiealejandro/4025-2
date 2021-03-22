47da811177f776da6d8c5a5e469b6d20c558bdfd
Jamie Chapman <34374063+Jamiealejandro@users.noreply.github.com>
2021-03-22 19:16:47 +0000

    FIXEDCAPS

--- client/app.js
+++ client/app.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import MainRouter from './mainrouter'
+import MainRouter from './MainRouter'
 import {BrowserRouter} from 'react-router-dom'
 import { ThemeProvider } from '@material-ui/styles'
 import theme from './theme'
