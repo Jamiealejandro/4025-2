d1e18fb801bd855b1c91eee95028a284c470cbbd
Jamie Chapman <34374063+Jamiealejandro@users.noreply.github.com>
2021-03-22 19:24:39 +0000

    mongodbfix

--- CWBAT.bat
+++ CWBAT.bat
@@ -1,1 +1,1 @@
-SET MONGODB_URI=mongodb+srv://jamiealejandro:Zulu1879@cluster0.rhco3.mongodb.net/CWDB?retryWrites=true&w=majority npm run-script development
+SET PORT=3000 JWT_SECRET="mysecret" MONGODB_URI="mongodb+srv://jamiealejandro:Zulu1879@cluster0.rhco3.mongodb.net/CWDB?retryWrites=true&w=majority" npm run-script development
