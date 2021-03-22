85930c8a8e1b16af903d6593717c4b9afab25997
Jamie Chapman <34374063+Jamiealejandro@users.noreply.github.com>
2021-03-22 16:23:57 +0000

    appjsfix

--- frontpage.js
+++ frontpage.js
@@ -4,6 +4,8 @@ export default ({markup, css}) => {
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> Roman History Lover's Forum </title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
 
   </head>
   <header>
@@ -13,7 +15,7 @@ export default ({markup, css}) => {
 
 </header>
     <body style="margin:0">
-      
+
     <body>
       <div id="root">${markup}</div>
       <style id="jss-server-side">${css}</style>
